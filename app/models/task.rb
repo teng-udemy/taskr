f1ed56493d149c40cb6e98f96977c3ed20dd0f2a
Teng Rails <tgisip@yahoo.com>
2021-04-13 19:54:48 -0500

    Added tasks table and model

---
+++ app/models/task.rb
@@ -0,0 +1,3 @@
+class Task < ApplicationRecord
+    add_column :tasks, :notes, :text
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_035712) do
+ActiveRecord::Schema.define(version: 2021_04_13_223552) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
